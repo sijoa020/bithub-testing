3a95d94abbf22e9597047fc29bbf22e3c59fd99c
sijoa020 <65156626+sijoa020@users.noreply.github.com>
2020-05-11 10:02:48 +0200

    music

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("C - D E - G - - ", 120)
+    }
     basic.showLeds(`
         . . . . .
         . # . # .
